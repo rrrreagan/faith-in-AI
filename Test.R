792a9ab6b91c562aa0e1526b3dcbe41cea2651c2
rrrreagan <rince@uw.edu>
2025-02-07 13:15:01 -0800

    Added R Files

---
+++ Test.R
@@ -0,0 +1,11 @@
+#Reagan Ince
+#Faith in AI Webscraping
+#currently unable to access Facebook dev account due to security issue
+
+# install packages
+library(RedditExtractoR)
+library(Rfacebook)
+library(httr)
+library(jsonlite)
+
+
